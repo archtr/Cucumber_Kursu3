1f2421f43d16be97ecc72b5507bdd07097f0bf0a
TechnoStudy <ismettechno@gmail.com>
2022-08-29 18:50:41 +0100

    Extent Report 4 writeExtentReport

--- src/test/java/Runners/_07_TestRunnerExtentReport.java
+++ src/test/java/Runners/_07_TestRunnerExtentReport.java
@@ -1,8 +1,10 @@
 package Runners;
 
+import com.aventstack.extentreports.service.ExtentService;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Listeners;
 
 
@@ -13,4 +15,13 @@ import org.testng.annotations.Listeners;
 )
 @Listeners({ExtentITestListenerClassAdapter.class})
 public class _07_TestRunnerExtentReport extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public static void writeExtentReport() {
+        ExtentService.getInstance().setSystemInfo("User Name", "İsmet Temur");
+        ExtentService.getInstance().setSystemInfo("Application Name", "Campus");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name").toString());
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+    }
 }
