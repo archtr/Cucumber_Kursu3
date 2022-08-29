1f947f53be7a88b4d860684a42f14f82d0a587de
TechnoStudy <ismettechno@gmail.com>
2022-08-29 19:30:30 +0100

    Extent Report 5 masterthought

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 
