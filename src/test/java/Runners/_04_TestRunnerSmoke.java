c98b3dce529426b8eb9a41fdce629e9f1d539f35
TechnoStudy <ismettechno@gmail.com>
2022-08-30 18:41:01 +0100

    Extent Report Parallel ve Cross browser Testing

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -7,8 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
-        glue = {"StepDefinitions"},
-        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+        glue = {"StepDefinitions"}
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 
--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -4,54 +4,89 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class GWD {
-
     public static WebDriver driver;
 
-    public static WebDriver getDriver()
-    {
-       if (driver == null) {
+    //Bana neler lazım:  1 browser tipi lazım burada ona göre oluşturucam
+    // her bir paralel çalışan süreç için sadece o sürece özel static bir değişken lazım
+    // çünkü runner classdaki işaret edilen tüm senaryolarda aynısını çalışması lazım.
+    // demekki her pipeline için (Local) ve de ona özel static bir drivera ihtiyacım var
+    //donanımdaki adı pipeline , yazılımdaki adı Thread , paralel çalışan her bir süreç
 
-           Logger.getLogger("").setLevel(Level.SEVERE);
-           System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "Error");
-           System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>(); // WebDriver 1 WebbDriver 2
+    public static ThreadLocal<String> threadBrowserName = new ThreadLocal<>(); // chrome , firefox ...
+
+    // threadDriver.get() -> ilgili tread deki driveri verecek
+    // threadDriver.set(driver) -> ilgili thread e driver set ediliyor.
+
+    public static WebDriver getDriver() {
+        // extend report türkçe bilg çalışmaması sebebiyle kondu
+        Locale.setDefault(new Locale("EN"));
+        System.setProperty("user.language", "EN");
+
+        Logger.getLogger("").setLevel(Level.SEVERE);
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "Error");
 
 
-           WebDriverManager.chromedriver().setup();
-           driver = new ChromeDriver();
+        if (threadDriver.get() == null) {
 
-           // firefox
-//           WebDriverManager.firefoxdriver().setup();
-//           driver=new FirefoxDriver();
-       }
+            String browserName = threadBrowserName.get(); // bu threaddeki browsername i verdi.
+            switch (browserName) {
+                case "chrome":
+                    System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+                    WebDriverManager.chromedriver().setup();
+                    threadDriver.set(new ChromeDriver()); // bu thread e chrome istenmişşse ve yoksa bir tane ekleniyor
+                    break;
 
-        return driver;
-    }
+                case "firefox":
+                    System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
+                    WebDriverManager.firefoxdriver().setup();
+                    threadDriver.set(new FirefoxDriver());  // bu thread e firefox istenmişşse ve yoksa bir tane ekleniyor
+                    break;
 
-    public static void quitDriver()
-    {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    threadDriver.set(new SafariDriver());
+                    break;
+
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    threadDriver.set(new EdgeDriver());
+                    break;
+            }
         }
 
-        if (driver != null) { // driver varsa
-            driver.quit();
-            driver=null;
-        }
+        return threadDriver.get();
     }
 
-    public static void Bekle(int saniye)
-    {
+    public static void quitDriver() {
+        Bekle(5);
+
+        if (threadDriver.get() != null) { // driver varsa
+            threadDriver.get().quit();
+
+            WebDriver driver = threadDriver.get();
+            driver = null;
+            threadDriver.set(driver); // tekrar gelirse için boş olmuş olsun
+        }
+
+    }
+
+
+
+    public static void Bekle(int saniye) {
         try {
-            Thread.sleep(saniye*1000);
+            Thread.sleep(saniye * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
